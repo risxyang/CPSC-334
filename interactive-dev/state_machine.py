5b6f8c92b93a5b5b9cff3230ccf938b8f8944250
risxyang <christine.yang@yale.edu>
2021-09-28 01:33:29 -0400

    added some more states

--- interactive-dev/state_machine.py
+++ interactive-dev/state_machine.py
@@ -11,7 +11,7 @@ b_prevstate = 0
 s_prevstate = 0
 j_x_prevstate = 0
 j_y_prevstate = 0
-j_sw_currstate = 0
+j_sw_prevstate = 0
 
 val_x = 0
 val_y = 0
@@ -38,7 +38,19 @@ while True: #change this eventually to exit program upon button being held in ce
 			print("Switch off")
 			print("Current State: 0")
 	elif state == 1:
-		if b.value == 1 and b_prevstate !=1:
+		if s.value == 0 and s_prevstate != 0:
+			state = 0
+			s_prevstate = 0 #mark switch as turned off
+			print("Switch off")
+			print("Current State: 0")
+		elif j_sw.value == 1 and j_sw_prevstate != 1:
+			val_x = 0
+			val_y = 0
+			j_sw_prevstate = 1 #mark j_sw as having been pressed
+			print("Reset Values: x and y are: ", val_x, val_y)
+		elif j_sw.value == 0 and j_sw_prevstate != 0:
+			j_sw_prevstate = 0 #mark j_sw as having been released
+		elif b.value == 1 and b_prevstate !=1:
 			state = 2
 			b_prevstate = 1 #mark button as being pressed
 			print("Button pressed")
@@ -57,7 +69,19 @@ while True: #change this eventually to exit program upon button being held in ce
 			elif j_y.value == 0 and j_y_prevstate != 0:
 				j_y_prevstate = 0 #mark joystick y as just having been released
 	elif state == 2:
-		if b.value == 0 and b_prevstate != 0:
+		if s.value == 0 and s_prevstate != 0:
+                        state = 0
+                        s_prevstate = 0 #mark switch as turned off
+                        print("Switch off")
+                        print("Current State: 0")
+		elif j_sw.value == 1 and j_sw_prevstate != 1:
+			val_x = 0
+			val_y = 0
+			j_sw_prevstate = 1 #mark j_sw as having been pressed
+			print("Reset Values: x and y are: ", val_x, val_y)
+		elif j_sw.value == 0 and j_sw_prevstate != 0:
+                	j_sw_prevstate = 0 #mark j_sw as having been released
+		elif b.value == 0 and b_prevstate != 0:
 			state = 1
 			b_prevstate = 0 #mark button as no longer pressed
 			print("Button released")
