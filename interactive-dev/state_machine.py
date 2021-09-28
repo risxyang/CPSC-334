211e22488494a3b3968fdd8eb4c58cc06c84d769
risxyang <christine.yang@yale.edu>
2021-09-28 11:46:21 -0400

    added program-exit from state 0; changed output to better communicate state transitions

--- interactive-dev/state_machine.py
+++ interactive-dev/state_machine.py
@@ -1,12 +1,17 @@
 from gpiozero import Button
+import os
 
 j_sw = Button(22) #click (downwards pressure) on analog joystick
 j_y = Button(27) #y axis of analog joystick
 j_x = Button(17) #x axis of the analog joystick
-b = Button(16) #momentary button
+b = Button(16, hold_time = 5) #momentary button
 s = Button(26) #toggle switch
 
 state = 0 #start off state machine
+# state 0: switch off
+# state 1: switch on; toggling joystick decrements x/y vals
+#state 2: switch on, button held: toggling joystick increments x/y vals
+
 b_prevstate = 0
 s_prevstate = 0
 j_x_prevstate = 0
@@ -15,6 +20,10 @@ j_sw_prevstate = 0
 
 val_x = 0
 val_y = 0
+
+def shutdown():
+	print("Button pressed: exiting")
+	os._exit(0)
 
 if s.value == 0:
 	state = 0
@@ -29,19 +38,30 @@ while True: #change this eventually to exit program upon button being held in ce
 		if s.value == 1 and s_prevstate != 1:
 			state = 1
 			s_prevstate = 1 #mark switch as turned on
-			print("Switch on")
-			print("Current State: 1")
-	elif state == 0:
-		if s.value == 0 and s_prevstate != 0:
+			print("Switch on: transitioning to state 1")
+			print("Current state: 1")
+		elif s.value == 0 and s_prevstate != 0:
 			state = 0
 			s_prevstate = 0 #mark switch as turned off
-			print("Switch off")
+			print("Switch off: transitioning to state 0")
 			print("Current State: 0")
+		elif j_x.value == 1 and j_x_prevstate != 1:
+			j_x_prevstate = 1 #mark j_x as having been toggled to
+			print("Joystick locked")
+		elif j_x.value == 0 and j_x_prevstate != 0:
+			j_x_prevstate = 0 #mark j_x as having been released
+		elif j_y.value == 1 and j_y_prevstate != 1:
+			j_y_prevstate = 1 #mark j_y as having been toggled to
+			print("Joystick locked")
+		elif j_y.value == 0 and j_y_prevstate != 0:
+			j_y_prevstate = 0 #mark j_y as having been released
+		else:
+			b.when_held = shutdown
 	elif state == 1:
 		if s.value == 0 and s_prevstate != 0:
 			state = 0
 			s_prevstate = 0 #mark switch as turned off
-			print("Switch off")
+			print("Switch off: transitioning to state 0")
 			print("Current State: 0")
 		elif j_sw.value == 1 and j_sw_prevstate != 1:
 			val_x = 0
@@ -53,7 +73,7 @@ while True: #change this eventually to exit program upon button being held in ce
 		elif b.value == 1 and b_prevstate !=1:
 			state = 2
 			b_prevstate = 1 #mark button as being pressed
-			print("Button pressed")
+			print("Button pressed: transitioning to state 2")
 			print("Current State: 2")
 		else: #we are in state 1 and there was no transition this time around
 			if j_x.value == 1 and j_x_prevstate != 1:
@@ -72,7 +92,7 @@ while True: #change this eventually to exit program upon button being held in ce
 		if s.value == 0 and s_prevstate != 0:
                         state = 0
                         s_prevstate = 0 #mark switch as turned off
-                        print("Switch off")
+                        print("Switch off: transitioning to state 0")
                         print("Current State: 0")
 		elif j_sw.value == 1 and j_sw_prevstate != 1:
 			val_x = 0
@@ -84,7 +104,7 @@ while True: #change this eventually to exit program upon button being held in ce
 		elif b.value == 0 and b_prevstate != 0:
 			state = 1
 			b_prevstate = 0 #mark button as no longer pressed
-			print("Button released")
+			print("Button released: transitioning to state 1")
 			print("Current State: 1")
 		else: #we are in state 2 and there was no transition this time around
 			if j_x.value == 1 and j_x_prevstate != 1:
