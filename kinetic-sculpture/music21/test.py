850d12eb51221366b3682debc72afa15c8cb18b8
Christine Yang <christine.yang@yale.edu>
2021-11-11 01:32:52 -0500

    pyserial to arduino progress

--- kinetic-sculpture/music21/test.py
+++ kinetic-sculpture/music21/test.py
@@ -5,7 +5,12 @@ import time
 start = time.time() 
 
 # file = converter.parse('ballade1.mid')
-os.system('echo "" > score')
+# os.system('echo "" > score')
+
+arduino = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=115200, timeout=.1)
+def write(x):
+    arduino.write(x.encode("utf-8"))
+
 
 fp = 'ballade1.mid'
 mf = midi.MidiFile()
@@ -16,43 +21,64 @@ len(mf.tracks)
 s = midi.translate.midiFileToStream(mf)
 measures = s.getElementsByClass(stream.Part)[0].getElementsByClass(stream.Measure)
 # print(s.getElementsByClass(stream.Part)[0].getElementsByClass(stream.Tempo))
-
-bpm = 142.0
-b = 60.0 / 142.0
+bpm = 100.0
+# print(bpm)
+#bpm = 142.0
+# b = 60.0 / bpm
 # print(b)
 
 for i,m in enumerate(measures):
-    print(i+1, m, m.offset)
+    # print(i+1, m, m.offset)
     # m = m.getElementsByClass(stream.Voice)[0]
     v =  m.getElementsByClass(stream.Voice)
     if len(v) > 0:
         m = v[0]
-    print("notes", len(m.getElementsByClass(note.Note)))
-    print("chords", len(m.getElementsByClass(chord.Chord)))
-    print("rests", len(m.getElementsByClass(note.Rest)))
+    # print("notes", len(m.getElementsByClass(note.Note)))
+    # print("chords", len(m.getElementsByClass(chord.Chord)))
+    # print("rests", len(m.getElementsByClass(note.Rest)))
     for elem in m:
+        currtime = time.time()
+        if 'tempo' in elem.classes or 'MetronomeMark' in elem.classes:
+            bpm = elem.number
+            print(bpm)
         if 'Note' in elem.classes:
-            print("i am a note")
-            print(elem.name, elem.octave, elem.offset + m.offset)
+            # print("i am a note")
+            # write(str(elem.offset + m.offset) + ",note," + elem.name + ":" + str(elem.octave))
+            write("0," + elem.name + ":" + str(elem.octave) + "\n")
         elif 'Chord' in elem.classes:
-            print("I am a chord")
+            # print("I am a chord")
+            pitchstr = ''
             for pitch in elem.pitches:
-                print("i am a note in a chord", elem.offset + m.offset)
-                print(pitch.name, pitch.octave)
+                # print("i am a note in a chord", elem.offset + m.offset)
+                # print(pitch.name, pitch.octave)
+                if pitchstr == '':
+                    pitchstr += pitch.name + ":" + str(pitch.octave)
+                else:
+                    pitchstr += "," + pitch.name + ":" + str(pitch.octave)
+                #write(str(elem.offset + m.offset) +  ",chord," + pitchstr)
+                write("1," + pitchstr + "\n")
+
         elif 'Rest' in elem.classes:
-            print("I am a rest", elem.offset + m.offset)
-        # print(elem, elem.offset)
+            # print("I am a rest", elem.offset + m.offset)
+            # write(str(elem.offset + m.offset) + ",rest")
+            write("2" + "\n")
+        
+        print(elem, elem.duration.quarterLength * (60.0 / bpm))
         # os.system('echo '+ str(elem) + "," + elem.offset + '>> score')
+        while(1):
+            if(time.time() - currtime > elem.duration.quarterLength * (60.0 / bpm)):
+                break
 
 # # measures[239].show()
 
 
-# arduino = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=115200, timeout=.1)
-# def write_read(x):
+# def write(x):
 #     arduino.write(bytes(x, 'utf-8'))
-#     time.sleep(0.05)
-#     data = arduino.readline()
-#     return data
+    # time.sleep(0.05)
+    # data = arduino.readline()
+    # return data
+
+
 # while True:
 #     num = input("Enter a number: ") # Taking input from user
 #     value = write_read(num)
--- kinetic-sculpture/pyserial/pyserial.ino
+++ kinetic-sculpture/pyserial/pyserial.ino
@@ -1,10 +1,27 @@
-int x;
+String x;
 void setup() {
  Serial.begin(115200);
  Serial.setTimeout(1);
 }
 void loop() {
- while (!Serial.available());
- x = Serial.readString().toInt();
- Serial.print(x + 1);
+// while (!Serial.available());
+// x = Serial.readStringUntil('\n');
+//// Serial.print(x + 1);
+//  Serial.println(x);
+
+  String inData = "";
+  while (Serial.available() > 0)
+    {
+        char recieved = Serial.readString();
+        inData += recieved; 
+
+        // Process message when new line character is recieved
+        if (recieved == '\n')
+        {
+            Serial.print("Arduino Received: ");
+            Serial.print(inData);
+
+            inData = ""; // Clear recieved buffer
+        }
+    }
 }
