2a704e8cdfb99039a5c8593640e408e7755b9f16
Christine Yang <christine.yang@yale.edu>
2021-11-11 00:37:35 -0500

    parsing midi to music21 stream

--- kinetic-sculpture/music21/test.py
+++ kinetic-sculpture/music21/test.py
@@ -1,7 +1,11 @@
 from music21 import *
 import os
+import serial
+import time
+start = time.time() 
 
 # file = converter.parse('ballade1.mid')
+os.system('echo "" > score')
 
 fp = 'ballade1.mid'
 mf = midi.MidiFile()
@@ -11,6 +15,12 @@ mf.close()
 len(mf.tracks)
 s = midi.translate.midiFileToStream(mf)
 measures = s.getElementsByClass(stream.Part)[0].getElementsByClass(stream.Measure)
+# print(s.getElementsByClass(stream.Part)[0].getElementsByClass(stream.Tempo))
+
+bpm = 142.0
+b = 60.0 / 142.0
+# print(b)
+
 for i,m in enumerate(measures):
     print(i+1, m, m.offset)
     # m = m.getElementsByClass(stream.Voice)[0]
@@ -21,5 +31,29 @@ for i,m in enumerate(measures):
     print("chords", len(m.getElementsByClass(chord.Chord)))
     print("rests", len(m.getElementsByClass(note.Rest)))
     for elem in m:
-        print(elem, elem.offset)
-    # m.show('text')
+        if 'Note' in elem.classes:
+            print("i am a note")
+            print(elem.name, elem.octave, elem.offset + m.offset)
+        elif 'Chord' in elem.classes:
+            print("I am a chord")
+            for pitch in elem.pitches:
+                print("i am a note in a chord", elem.offset + m.offset)
+                print(pitch.name, pitch.octave)
+        elif 'Rest' in elem.classes:
+            print("I am a rest", elem.offset + m.offset)
+        # print(elem, elem.offset)
+        # os.system('echo '+ str(elem) + "," + elem.offset + '>> score')
+
+# # measures[239].show()
+
+
+# arduino = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=115200, timeout=.1)
+# def write_read(x):
+#     arduino.write(bytes(x, 'utf-8'))
+#     time.sleep(0.05)
+#     data = arduino.readline()
+#     return data
+# while True:
+#     num = input("Enter a number: ") # Taking input from user
+#     value = write_read(num)
+#     print(value) # printing the value
