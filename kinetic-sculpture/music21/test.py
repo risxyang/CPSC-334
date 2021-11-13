6113f96868c96ec18128079b021e105ad54a7742
Christine Yang <christine.yang@yale.edu>
2021-11-12 23:04:23 -0500

    arduino input read

--- kinetic-sculpture/music21/test.py
+++ kinetic-sculpture/music21/test.py
@@ -2,10 +2,10 @@ from music21 import *
 import os
 import serial
 import time
+import random
+
 start = time.time() 
 
-# file = converter.parse('ballade1.mid')
-# os.system('echo "" > score')
 
 arduino = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=115200, timeout=.1)
 def write(x):
@@ -20,66 +20,34 @@ mf.close()
 len(mf.tracks)
 s = midi.translate.midiFileToStream(mf)
 measures = s.getElementsByClass(stream.Part)[0].getElementsByClass(stream.Measure)
-# print(s.getElementsByClass(stream.Part)[0].getElementsByClass(stream.Tempo))
 bpm = 100.0
-# print(bpm)
-#bpm = 142.0
-# b = 60.0 / bpm
-# print(b)
 
 for i,m in enumerate(measures):
-    # print(i+1, m, m.offset)
-    # m = m.getElementsByClass(stream.Voice)[0]
     v =  m.getElementsByClass(stream.Voice)
     if len(v) > 0:
         m = v[0]
-    # print("notes", len(m.getElementsByClass(note.Note)))
-    # print("chords", len(m.getElementsByClass(chord.Chord)))
-    # print("rests", len(m.getElementsByClass(note.Rest)))
     for elem in m:
         currtime = time.time()
         if 'tempo' in elem.classes or 'MetronomeMark' in elem.classes:
             bpm = elem.number
             print(bpm)
         if 'Note' in elem.classes:
-            # print("i am a note")
-            # write(str(elem.offset + m.offset) + ",note," + elem.name + ":" + str(elem.octave))
             write("0," + elem.name + ":" + str(elem.octave) + "\n")
         elif 'Chord' in elem.classes:
-            # print("I am a chord")
             pitchstr = ''
             for pitch in elem.pitches:
-                # print("i am a note in a chord", elem.offset + m.offset)
-                # print(pitch.name, pitch.octave)
                 if pitchstr == '':
                     pitchstr += pitch.name + ":" + str(pitch.octave)
                 else:
                     pitchstr += "," + pitch.name + ":" + str(pitch.octave)
-                #write(str(elem.offset + m.offset) +  ",chord," + pitchstr)
-                write("1," + pitchstr + "\n")
+                write("1," + str(len(elem.pitches)) + ",'" + pitchstr + "\n")
 
         elif 'Rest' in elem.classes:
-            # print("I am a rest", elem.offset + m.offset)
-            # write(str(elem.offset + m.offset) + ",rest")
             write("2" + "\n")
         
         print(elem, elem.duration.quarterLength * (60.0 / bpm))
-        # os.system('echo '+ str(elem) + "," + elem.offset + '>> score')
         while(1):
             if(time.time() - currtime > elem.duration.quarterLength * (60.0 / bpm)):
                 break
 
 # # measures[239].show()
-
-
-# def write(x):
-#     arduino.write(bytes(x, 'utf-8'))
-    # time.sleep(0.05)
-    # data = arduino.readline()
-    # return data
-
-
-# while True:
-#     num = input("Enter a number: ") # Taking input from user
-#     value = write_read(num)
-#     print(value) # printing the value
--- kinetic-sculpture/pyserial/pyserial.ino
+++ kinetic-sculpture/pyserial/pyserial.ino
@@ -1,27 +1,25 @@
 String x;
+
 void setup() {
  Serial.begin(115200);
- Serial.setTimeout(1);
+ Serial.setTimeout(2);
 }
 void loop() {
-// while (!Serial.available());
-// x = Serial.readStringUntil('\n');
-//// Serial.print(x + 1);
-//  Serial.println(x);
-
-  String inData = "";
-  while (Serial.available() > 0)
-    {
-        char recieved = Serial.readString();
-        inData += recieved; 
-
-        // Process message when new line character is recieved
-        if (recieved == '\n')
-        {
-            Serial.print("Arduino Received: ");
-            Serial.print(inData);
-
-            inData = ""; // Clear recieved buffer
-        }
-    }
+ while (!Serial.available());
+ x = Serial.readStringUntil('\n');
+// Serial.print(x + 1);
+  Serial.println(x);
+  if(x[0].toInt() == 0)
+  {
+    //note
+  }
+  else if(x[0].toInt() == 1)
+  {
+    //chord
+  
+  }
+  else if(x[0].toInt() == 2)
+  {
+    //rest
+  }
 }
